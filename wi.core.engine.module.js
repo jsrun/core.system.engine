57f24328636268f57d4a710d8400ae617c426c78
Andre <Andre@DESKTOP-EVCUACT>
2017-01-14 18:32:01 -0200

    compatibily changes

--- wi.core.engine.module.js
+++ wi.core.engine.module.js
@@ -18,13 +18,15 @@ module.exports = {
      * @type object
      */
     assets: {
-        css: [__dirname + "/malihu/malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.min.css",
+        css: [__dirname + "/malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.min.css",
+              __dirname + "/tooltipster/dist/css/tooltipster.bundle.min.css",
               __dirname + "/jquery-ui-1.12.1.min.css",
               __dirname + "/wi.core.engine.style.css"],
         js: [__dirname + "/jquery-3.1.1.min.js", 
              __dirname + "/jquery-ui-1.12.1.min.js", 
-             __dirname + "/malihu/malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.concat.min.js",
-             __dirname + "/desandro/draggabilly/dist/draggabilly.pkgd.min.js",
+             __dirname + "/malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.concat.min.js",
+             __dirname + "/tooltipster/dist/js/tooltipster.bundle.min.js",
+             __dirname + "/draggabilly/dist/draggabilly.pkgd.min.js",
              __dirname + "/wi.core.engine.event.js"]
     }
 };
